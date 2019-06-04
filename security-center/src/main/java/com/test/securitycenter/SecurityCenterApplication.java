9da21ab22483e288e0a9a0b6a6864eca059a3d2c
wuxingle <1546752823@qq.com>
2019-06-04 16:19:02 +0800

    第一次添加

---
+++ security-center/src/main/java/com/test/securitycenter/SecurityCenterApplication.java
@@ -0,0 +1,13 @@
+package com.test.securitycenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityCenterApplication.class, args);
+    }
+
+}
